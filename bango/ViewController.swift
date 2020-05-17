964ebb842e65c71fbe2dd2c0c0f1a28451566354
sen1000-byte <nishiwaki.chihiro@gmail.com>
2020-05-17 18:21:02 +0900

    リトライ

--- bango/ViewController.swift
+++ bango/ViewController.swift
@@ -10,26 +10,61 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet var number: UILabel!
-    @IBOutlet var name: UILabel!
+    @IBOutlet var banmeLabel: UILabel!
+    @IBOutlet var nameLabel: UILabel!
     
-    @IBOutlet var ichiban: UIButton!
-    @IBOutlet var niban: UIButton!
-    @IBOutlet var sanban: UIButton!
-    @IBOutlet var mae: UIButton!
-    @IBOutlet var tugi: UIButton!
-
+    var number: Int = 0
+    
+    let banme = ["1", "2", "3", "4", "5"]
+    let name = ["one", "two", "three", "four", "five"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
     
-
-    
-    if{
-    
+    func view() {
+        banmeLabel.text = banme[number]
+        nameLabel.text = name[number]
+        
     }
+    
+    @IBAction func ichiban() {
+        number = 0
+        view()
+    }
+    
+    @IBAction func niban() {
+        number = 1
+        view()
+    }
+    
+    @IBAction func sanban() {
+        number = 2
+        view()
+    }
+    
+    @IBAction func back() {
+        if number >= 1 {
+            number -= 1
+            view()
+        }else{
+            number = 4
+            view()
+        }
+        
+    }
+    
+    @IBAction func next() {
+        if number <= 3{
+            number += 1
+            view()
+        } else{
+            number = 0
+            view()
+        }
+    }
+    
+   
 }
 
