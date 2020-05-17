49c61321b13f0ba2b3bd35ff58bab64a1d2ea636
sen1000-byte <nishiwaki.chihiro@gmail.com>
2020-05-17 14:12:02 +0900

    Initial Commit

---
+++ bango/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  bango
+//
+//  Created by Chihiro Nishiwaki on 2020/05/17.
+//  Copyright © 2020 com.litech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
